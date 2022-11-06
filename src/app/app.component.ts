86557f1fac9f4ef5489e8a8c602079b638ce8146
franklin Nkemjika <72893125+Franklin-Nkemjika@users.noreply.github.com>
2022-11-06 08:07:29 -0700

    initialize counter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'javaScript-project';
+  initModel: number = 0;
+
+  plusOrMinusClick(msg: string) {
+    let model = this.initModel;
+    let currentView = model;
+    let message = msg;
+    function dispatch(msg:string) {
+      model = update(msg, model);
+      const updatedView = model;
+      currentView = updatedView;
+    }
+    dispatch(message)
+    this.initModel=currentView;
+  }
+}
+
+function update(msg: any, model: number) {
+  switch (msg) {
+    case 'plus':
+      return model + 1;
+    case 'minus':
+      return model - 1;
+    default:
+      return model;
+  }
 }
